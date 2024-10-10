afa074cb5b418e82e325887d8c96e9b5f627ab6c
Samuel CURRAN <samuelcurran64100@gmail.com>
2024-10-10 16:14:26 +0200

    Deploiement

---
+++ docs/server/chunk-7K5CBJIP.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a as p,b as r,c as i,d as n,e as m,j as a,m as f,o as s,p as c,s as l,y as d}from"./chunk-EQ4OSL3J.mjs";var o=class e{title="portfolio";static \u0275fac=function(t){return new(t||e)};static \u0275cmp=p({type:e,selectors:[["app-root"]],standalone:!0,features:[m],decls:2,vars:0,template:function(t,R){t&1&&(r(0,"h1"),n(1,"Testttt"),i())}})};var u=[];var C={providers:[a({eventCoalescing:!0}),d(u),c()]};var A={providers:[l()]},g=f(C,A);var x=()=>s(o,g),q=x;export{q as a};
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-7K5CBJIP.mjs";import"./chunk-EQ4OSL3J.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
