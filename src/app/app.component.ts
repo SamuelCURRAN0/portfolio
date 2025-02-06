a8c473a470dc32faa7804fb4c2c47a4cd0fc6102
Samuel <samuelcurran64100@gmail.com>
2025-02-06 08:55:33 +0100

    correction erreur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit,Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AProposComponent } from './components/a-propos/a-propos.component';
 import { BoxComponent } from './components/box/box.component';
@@ -30,9 +31,12 @@ import * as AOS from 'aos';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit, AfterViewInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
   title = 'portfolio';
   ngOnInit() {
-    AOS.init();
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init({ once: true, duration: 1000 });
+    }
   }
   ngAfterViewInit() {
     AOS.refresh();
