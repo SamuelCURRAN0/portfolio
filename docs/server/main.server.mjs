7301f281883c39155fa128716d9dba3d626a4db5
samuel <samuelcurran64100@gmail.com>
2025-03-16 12:59:38 +0100

    Updated build for GitHub Pages

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-5N426DKT.mjs";import"./chunk-CN4QY5CJ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-XKGGP3AJ.mjs";import"./chunk-C7UQST7K.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
