ca0225c83266ad79ddbcf8ddd15ac598703de0c5
Samuel <samuelcurran64100@gmail.com>
2025-07-26 21:00:17 +0200

    Angular20

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,11 +1,15 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import { appConfig } from './app.config';
+import { ApplicationConfig } from '@angular/core';
+import { provideServerRendering } from '@angular/ssr';
+import { provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { routes } from './app.routes';
 
-const serverConfig: ApplicationConfig = {
+export const config: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
     provideServerRendering()
   ]
 };
-
-export const config = mergeApplicationConfig(appConfig, serverConfig);
