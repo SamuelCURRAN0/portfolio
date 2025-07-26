11bab47606e733719aed5410231b6d0dd8ab23d9
Samuel <samuelcurran64100@gmail.com>
2025-07-26 20:26:32 +0200

    update pack json

--- src/app/components/projets-liste/projets-liste.component.ts
+++ src/app/components/projets-liste/projets-liste.component.ts
@@ -3,13 +3,12 @@ import { Component, HostListener, Output, EventEmitter } from '@angular/core';
 import { Project } from '../../models/project.model'; 
 import { ProjectTag } from '../../models/project-tag.enum'; 
 import { ProjetComponent } from '../projet/projet.component';
-import { ProjetDetailComponent } from '../projet-detail/projet-detail.component';
 import { TranslationContentService } from '../../services/translation-content.service';
 
 @Component({
   selector: 'app-projets-liste',
   standalone: true,
-  imports: [CommonModule, ProjetComponent, ProjetDetailComponent],
+  imports: [CommonModule, ProjetComponent],
   templateUrl: './projets-liste.component.html',
   styleUrls: ['./projets-liste.component.scss'] // Corrected to 'styleUrls'
 })
