33d361a9ba3dd88589181799069d74c1310b2dd1
Samuel <samuelcurran64100@gmail.com>
2025-08-04 11:33:32 +0200

    color project tag

--- src/app/models/project-tag.enum.ts
+++ src/app/models/project-tag.enum.ts
@@ -2,22 +2,21 @@ export enum ProjectTag {
     Angular = 'Angular', 
     C = 'C', 
     CPlus = 'C++',   
-    GestionDeProjet = 'Gestion de Projet', 
+    ProjectManagement = 'Gestion de Projet', 
     JavaScript = 'JavaScript', 
     Java = 'Java',  
     Laravel = 'Laravel',
     mySQL = 'mySQL',
     PHP = 'PHP'
 }
-
 export const statusColors: Record<ProjectTag, string> = {
-  [ProjectTag.Angular]: '#ff4d4d',
-  [ProjectTag.C]: '#4d94ff',
-  [ProjectTag.CPlus]: '#9966ff',
-  [ProjectTag.GestionDeProjet]: '#ffcc4d',
-  [ProjectTag.JavaScript]: '#ffe04d',
-  [ProjectTag.Java]: '#66ff66',
-  [ProjectTag.Laravel]: '#ff99cc',
-  [ProjectTag.mySQL]: '#cc9966',
-  [ProjectTag.PHP]: '#4F5B93',
+  [ProjectTag.Angular]: '#e53935',        // rouge plus profond
+  [ProjectTag.C]: '#2563eb',              // bleu plus foncé
+  [ProjectTag.CPlus]: '#7c3aed',          // violet foncé
+  [ProjectTag.ProjectManagement]: '#f59e0b', // orange ambré foncé
+  [ProjectTag.JavaScript]: '#fbbf24',     // ambré foncé au lieu de jaune
+  [ProjectTag.Java]: '#16a34a',           // vert foncé
+  [ProjectTag.Laravel]: '#db2777',        // rose foncé
+  [ProjectTag.mySQL]: '#b45309',          // brun foncé
+  [ProjectTag.PHP]: '#3b4c83',            // déjà assez foncé, gardé tel quel
 };
